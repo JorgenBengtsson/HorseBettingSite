aeb440c2af9c724fa6abe970d44fbc7d9345d560
Unknown <jorgen.bengtsson@sigma.se>
2019-10-04 19:11:04 +0200

    Adding Bet input controls

--- FrontEnd/src/Components/BodyContent/Races/SpecificRaceTable.js
+++ FrontEnd/src/Components/BodyContent/Races/SpecificRaceTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
+import BetInput from "../../Input/BetInput";
 
 export default class SpecificRaceTable extends Component {
   componentDidMount() {
@@ -10,40 +11,43 @@ export default class SpecificRaceTable extends Component {
   }
   render() {
     return (
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Horse</th>
-            <th>Jockey</th>
-            <th>Last race</th>
-            <th>Odds</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Lucky Charm</td>
-            <td>John Stone</td>
-            <td>3rd, 2019-09-14</td>
-            <td>4:1</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Ginger Roadster</td>
-            <td>Gordon Scott</td>
-            <td>DNF, 2019-09-14</td>
-            <td>12:1</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Blue Star</td>
-            <td>Silvia von Carlton</td>
-            <td>1st, 2019-09-14</td>
-            <td>1:1</td>
-          </tr>
-        </tbody>
-      </Table>
+      <>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Horse</th>
+              <th>Jockey</th>
+              <th>Last race</th>
+              <th>Odds</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Lucky Charm</td>
+              <td>John Stone</td>
+              <td>3rd, 2019-09-14</td>
+              <td>4:1</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Ginger Roadster</td>
+              <td>Gordon Scott</td>
+              <td>DNF, 2019-09-14</td>
+              <td>12:1</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Blue Star</td>
+              <td>Silvia von Carlton</td>
+              <td>1st, 2019-09-14</td>
+              <td>1:1</td>
+            </tr>
+          </tbody>
+        </Table>
+        <BetInput />
+      </>
     );
   }
 }
---
+++ FrontEnd/src/Components/Input/BetInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+export default class BetInput extends Component {
+  render() {
+    return (
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>Bet as winner</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Horse</Form.Label>
+            <Form.Control as="select">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Winnings</Form.Label>
+            <Form.Control disabled />
+          </Form.Group>
+        </Form.Row>
+
+        <Button variant="primary" type="submit">
+          Make Bet
+        </Button>
+      </Form>
+    );
+  }
+}
