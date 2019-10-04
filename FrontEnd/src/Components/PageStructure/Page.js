57e9699820532ae466c11e0395fc5bc579f2b83f
Unknown <jorgen.bengtsson@sigma.se>
2019-10-04 16:26:01 +0200

    Add Footer and page navigation logic

--- FrontEnd/src/Components/PageStructure/Page.js
+++ FrontEnd/src/Components/PageStructure/Page.js
@@ -8,12 +8,25 @@ import Races from "./../BodyContent/Races";
 import Registration from "./../BodyContent/Registration";
 import Profile from "./../BodyContent/Profile";
 
+const pages = [
+  { title: "Races", identifier: "Races" },
+  { title: "Registration", identifier: "Registration" },
+  { title: "Profile", identifier: "Profile" }
+];
+
 export default class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pageToDisplay: "Home" };
+  }
   render() {
     return (
       <div>
-        <Header />
-        <Body pageToDisplay="Home">
+        <Header
+          menuItems={pages}
+          onChange={pageTitle => this.setState({ pageToDisplay: pageTitle })}
+        />
+        <Body pageToDisplay={this.state.pageToDisplay}>
           <Home title="Home" />
           <Races title="Races" />
           <Registration title="Registration" />
